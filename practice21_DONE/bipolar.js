b1c760bf1cf89b1e5b344f9d352692eecd068afe
Epadilla1886 <ejpadilla1886@gmail.com>
2019-03-27 20:24:44 -0700

    completed lvl 1 p6

--- practice21_DONE/bipolar.js
+++ practice21_DONE/bipolar.js
@@ -7,3 +7,12 @@
 //     for(<initiator 1>,<initiator 2>; <check 1>,<check2>; <increment 1>,<increment 2>){
 //     //do work
 // }
+
+function biPolar(){
+    for(let test2 = 0, test1 =10; test2 <= 10; test1--, test2++){
+        console.log (test1+','+test2);
+    }
+}
+
+biPolar();
+
